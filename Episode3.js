1f9f2085b14cfbf663dd64e22697fa7177d716f5
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-07 21:50:16 +0000

    episode 3 completed

--- Episode3.js
+++ Episode3.js
@@ -10,3 +10,11 @@ console.log('First Verdict: ', firstVerdict);
 
 const secondVerdict = `The murderer is ${murderer}.`;
 console.log('Second Verdict: ', secondVerdict);
+
+
+// The answer is First Verdict: The murderer is Mrs Peacock
+// Second Verdict: The murderer is Professor Plum because:
+// I worked backward drom line 12 to line 11 - the name is Prof Plum
+// because his name is declare on line 1.
+// First verdict, line 9 leads to line 8, which feeds into the function
+// in line 3 where Mrs Peacock is declared as the murderer.
