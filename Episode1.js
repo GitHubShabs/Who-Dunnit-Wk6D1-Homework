dc3f7fcc06ea36d4fab7d247a15dfa6966747338
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-07 21:37:47 +0000

    episode1 commit

--- Episode1.js
+++ Episode1.js
@@ -10,3 +10,8 @@ const declareMurderer = function() {
 
 const verdict = declareMurderer();
 console.log(verdict);
+
+// I predicted the output would be "the murderer is Miss Scarlet"
+// because line 12 is called which leads to the function in line 11
+// This function is called on line 7 which leads to the line "the murderer is Miss Scarlet"
+// where the murderer is Miss Scarlet (line2)
