54ee8951ad45d052f07f5776e04c8136c8e89b64
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-07 22:28:32 +0000

    episode9 complete

--- Episode9.js
+++ Episode9.js
@@ -10,3 +10,6 @@ const declareMurderer = function() {
 
 const verdict = declareMurderer();
 console.log(verdict);
+
+// Im guessing the murderer is professor plum because his name was declared with
+// let function and is outermost globally available variable for murderer?
